b99a9ffa22595e451769714ca5715ca51fcbba22
Metatron Ray <nnltj2010@qq.com>
2023-08-24 12:08:14 +0800

    fix

--- service.sh
+++ service.sh
@@ -1,11 +1,12 @@
 #!/system/bin/sh
 
 #mount cgroup
-mkdir -p /sys/fs/cgroup/systemd
-mkdir -p /sys/fs/cgroup/devices
-
 mount -t tmpfs -o mode=755 tmpfs /sys/fs/cgroup
+
+mkdir -p /sys/fs/cgroup/devices
 mount -t cgroup -o devices cgroup /sys/fs/cgroup/devices
+
+mkdir -p /sys/fs/cgroup/systemd
 mount -t cgroup cgroup -o none,name=systemd /sys/fs/cgroup/systemd
 
 echo "Mounting Cgroup finished"
